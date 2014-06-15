bd4a78978b5f161e9b6b20fe7e1ba5d97e2f235c
csimon <csimon@4ce1f454-c4d6-4e58-b415-35f2e5b6321e>
2014-06-15 15:35:53 +0000

    integrate modification 3

--- trunk/SA2I/src/ca/etsmtl/log430/common/Resource.java
+++ trunk/SA2I/src/ca/etsmtl/log430/common/Resource.java
@@ -1,5 +1,8 @@
 package ca.etsmtl.log430.common;
 
+import java.util.Date;
+
+
 
 /**
  * This class defines the Resource object for the system. Besides the basic
@@ -33,6 +36,8 @@ package ca.etsmtl.log430.common;
 
 public class Resource {
 
+	public static final int MAX_WORK_CHARGE = 100;
+	
 	/**
 	 * Resource's last name
 	 */
@@ -56,21 +61,36 @@ public class Resource {
 	/**
 	 *  List of projects the resource is already allocated to
 	 */
-	private ProjectList alreadyAssignedProjectList = new ProjectList();
+	private ProjectList alreadyAssignedProjectList;
 
 	/**
 	 *  List of projects assigned to the resource in this session
 	 */
-	private ProjectList projectsAssignedList = new ProjectList();
+	private ProjectList projectsAssignedList;
 
+	private boolean overallocated;
+	
+	public Resource() {
+		overallocated = false;
+		alreadyAssignedProjectList = new ProjectList();
+		projectsAssignedList = new ProjectList();
+	}
+	
 	/**
 	 * Assigns a project to a resource.
 	 * 
 	 * @param project
 	 */
-	public void assignProject(Project project) {
-
-		getProjectsAssigned().addProject(project);
+	public boolean assignProject(Project project) {
+		
+		if(!this.getAllProjectAssigned().contains(project)){
+			if(!this.isAvailableForProject(project)){
+				this.overallocated = true;
+			}
+			return this.projectsAssignedList.addProject(project);
+		}
+		
+		return false;
 
 	}
 
@@ -121,5 +141,91 @@ public class Resource {
 	public ProjectList getProjectsAssigned() {
 		return projectsAssignedList;
 	}
+	
+	private ProjectList getAllProjectAssigned() {
+		ProjectList projectAssigned = new ProjectList();
+		
+		projectAssigned.addAll(this.getPreviouslyAssignedProjectList());
+		projectAssigned.addAll(this.getProjectsAssigned());
+		
+		return projectAssigned;
+	}
 
+	/**
+	 * 
+	 * @return the work charge previously assign for this resource (in percentage)
+	 */
+	public int getWorkChargePreviouslyAssigned() {
+		int chargeWorkPreviouslyAssigned = 0;
+		ProjectList projectList = this.getPreviouslyAssignedProjectList();
+		
+		if( projectList != null){
+			for (Project project : projectList) {
+				chargeWorkPreviouslyAssigned += project.getPriority().getRessourceCharge();
+			}
+		}
+		
+		return chargeWorkPreviouslyAssigned;
+	}
+
+	/**
+	 * 
+	 * @return the work charge currently assign for this resource (in percentage)
+	 */
+	public int getWorkChargeCurrentlyAssigned() {
+		
+		int chargeWorkCurrentlyAssigned = 0;
+		ProjectList projectList = this.getProjectsAssigned();
+		
+		if( projectList != null){
+			for (Project project : projectList) {
+				chargeWorkCurrentlyAssigned += project.getPriority().getRessourceCharge();
+			}
+		}
+		
+		return chargeWorkCurrentlyAssigned;
+	}
+	
+	/**
+	 * 
+	 * @return total work charge for this resource (in percentage)
+	 */
+	public int getWorkCharge() {
+		return getWorkChargePreviouslyAssigned() + getWorkChargeCurrentlyAssigned();
+	}
+	
+	/**
+	 * 
+	 * @return total work charge for a date for this resource (in percentage)
+	 */
+	public int getWorkCharge(Date d) {
+		return getWorkChargePreviouslyAssigned() + getWorkChargeCurrentlyAssigned();
+	}
+
+	
+	public boolean isAvailableForProject(Project project) {
+		
+		if(project != null) {
+			for (Project p : this.getProjectsAssigned()) {
+				if(p.getPeriode().contains(project.getStartDate())){
+					if(p.getPriority().getRessourceCharge() + project.getPriority().getRessourceCharge() > Resource.MAX_WORK_CHARGE){
+						return false;
+					}
+				}
+				
+				if(p.getPeriode().contains(project.getEndDate())){
+					if(p.getPriority().getRessourceCharge() + project.getPriority().getRessourceCharge() > Resource.MAX_WORK_CHARGE){
+						return false;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean isOverallocated(){
+		return overallocated;
+	}
+	
+	
 } // Resource class
